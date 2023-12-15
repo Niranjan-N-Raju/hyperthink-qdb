52a2cba7d5d5a144572812414272420a59ff12cf
Niranjan <niranjan.raju007@gmail.com>
2023-12-15 16:29:08 +0530

    Converted inline css to css modules

--- src/components/Blog/Post.js
+++ src/components/Blog/Post.js
@@ -13,22 +13,16 @@ const Post = ({ post }) => {
   return (
     <>
       <Link to={`/post-details/${post.id}`}>
-        <div style={{ display: "flex", color: "#000" }}>
+        <div className={classes.PostWrapper}>
           <div>
             <img src="http://via.placeholder.com/200x200" alt={post.title} />
           </div>
-          <div style={{ padding: "0 24px", width: "100%" }}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                width: "100%",
-              }}
-            >
-              <span className={classes.postHeading}>{post.title}</span>
+          <div className={classes.Content}>
+            <div className={classes.TextContent}>
+              <span className={classes.PostHeading}>{post.title}</span>
               <span>{date}</span>
             </div>
-            <div style={{ textAlign: "left", paddingTop: "8px" }}>
+            <div className={classes.Body}>
               <p>{post.body}</p>
               <p>
                 <Link to="/">Read More...</Link>
