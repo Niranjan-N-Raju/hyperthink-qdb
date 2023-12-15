faea4bf68f2f569dd2caf386cbfc8b69955a7a30
Niranjan <niranjan.raju007@gmail.com>
2023-12-15 15:59:26 +0530

    Updated package json file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { Layout, Menu, theme } from "antd";
+import { Route, Routes } from "react-router-dom";
+
+import HeaderLogo from "./components/HeaderLogo/HeaderLogo";
+import Header from "./components/Header/Header";
+import UserDetails from "./components/UserDetails/UserDetails";
+import CalendarComponent from "./components/Dashboard/Calendar";
+import BlogPageContent from "./components/BlogPageContent/BlogPageContent";
+import Overview from "./components/Dashboard/Overview";
+import PostDetails from "./components/PostDetails/PostDetails";
+import EditPost from "./components/PostDetails/EditPost";
+
+import { headerOptions as items } from "./config";
+import classes from "./App.module.scss";
+
+const { Content, Footer, Sider } = Layout;
+
+const App = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout hasSider>
+      <Sider
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          position: "fixed",
+          left: 0,
+          top: 0,
+          bottom: 0,
+          backgroundColor: colorBgContainer,
+          width: "220px",
+          minWidth: "220px",
+          maxWidth: "220px",
+        }}
+      >
+        <HeaderLogo />
+        <UserDetails />
+        <Menu theme="light" mode="inline" items={items} />
+      </Sider>
+      <Layout
+        style={{
+          marginLeft: 200,
+        }}
+      >
+        <Header />
+        <div className={classes.ContentWrapper}>
+          <Content
+            style={{
+              overflow: "initial",
+              borderRadius: 0,
+            }}
+          >
+            <div className={classes.Content}>
+              <Routes>
+                <Route path="/" element={"Welcome to our bank(QDB)"} />
+                <Route path="/dashboard" element={"Dashboard"} />
+                <Route path="/overview" element={<Overview />} />
+                <Route path="/calendar" element={<CalendarComponent />} />
+                <Route path="/blogs" element={<BlogPageContent />} />
+                <Route path="/post-details/:id" element={<PostDetails />} />
+                <Route path="/edit-post/:id" element={<EditPost />} />
+                <Route
+                  path="/blogs-all"
+                  element={<BlogPageContent type="all-posts" />}
+                />
+                <Route
+                  path="/blogs-latest"
+                  element={<BlogPageContent type="latest-posts" />}
+                />
+                <Route
+                  path="/blogs-archived"
+                  element={<BlogPageContent type="archived-posts" />}
+                />
+              </Routes>
+            </div>
+          </Content>
+        </div>
+        <Footer className={classes.FooterText}>
+          Design ©2023 Created by HyperThink
+        </Footer>
+      </Layout>
+    </Layout>
   );
-}
-
+};
 export default App;
